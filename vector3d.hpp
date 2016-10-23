84502ab61b845ba7489eeffebd93657c428757e8
Andrey543 <andrey_rybakov_97@mail.ru>
2016-10-23 14:38:52 +0300

    Update vector3d.hpp

--- vector3d.hpp
+++ vector3d.hpp
@@ -10,7 +10,20 @@ public:
     const static vector3d Z;
     const static vector3d ZERO;
 
-    // TODO: дописать
+
+    int x;
+    int y;
+    int z;
+
+
+
+    vector3d();
+    vector3d(int x,int y, int z);
+    vector3d operator+(vector3d v);
+    vector3d operator-(vector3d v);
+    bool operator==(vector3d v);
+    bool operator!=(vector3d v);
+
 
 };
 
